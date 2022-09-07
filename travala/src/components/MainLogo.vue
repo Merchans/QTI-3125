97542680255929f538d4d0cfad9f82407dbe8b43
Richard Markovič <richard.markovic@quanti.cz>
2022-09-07 08:03:51 +0200

    QTI-3125 point 2 in progress

---
+++ travala/src/components/MainLogo.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+    lightMode: {
+        type: Boolean,
+        default: false,
+    },
+    letter: {
+        type: String,
+        default: 'T',
+    },
+})
+</script>
+    
+<template>
+    <div class="main-logo" :class="{'main-logo--light': lightMode}">
+
+        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"
+            class="main-logo__img">
+            <rect width="50" height="50" rx="25" fill="white" fill-opacity="0.08" />
+            <text fill="currentcolor" xml:space="preserve" font-weight="900" letter-spacing="0em">
+                <tspan x="18.5" y="31.5455">{{ letter }}</tspan>
+            </text>
+        </svg>
+
+        <div class="main-logo__text">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "@/sass/components/main-logo.scss";
+</style>
+
---
+++ travala/src/components/MainNavigation.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+const props = defineProps({
+    showCta: {
+        type: Boolean,
+        required: false
+    }
+})
+</script>
+        
+<template>
+    <nav class="nav-bar">
+        <ul class="nav-bar__list" v-if="!props.showCta">
+            <li class="nav-bar__item">
+                <RouterLink to="/" class="nav-bar__link nav-bar__link--active">Home</RouterLink>
+            </li>
+            <li class="nav-bar__item">
+                <RouterLink to="/about" class="nav-bar__link">About</RouterLink>
+            </li>
+            <li class="nav-bar__item">
+                <RouterLink to="/destination" class="nav-bar__link">Destination</RouterLink>
+            </li>
+            <li class="nav-bar__item">
+                <RouterLink to="/contact" class="nav-bar__link">Contact</RouterLink>
+            </li>
+        </ul>
+        <ul class="nav-bar__cta" v-if="props.showCta">
+            <li class="nav-bar__item">
+                <RouterLink to="/register" class="nav-bar__link nav-bar__link--light">Register</RouterLink>
+            </li>
+            <li class="button">
+                <RouterLink to="/login">Login</RouterLink>
+            </li>
+        </ul>
+    </nav>
+</template>
+    
+<style lang="scss" scoped>
+@use "@/sass/components/navigation.scss";
+</style>
+    
+    
